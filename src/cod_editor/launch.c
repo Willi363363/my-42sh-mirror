e3b348a18861aed3fc9922e18ca4fd75adbeee3c
Yaël Pierre <yael.pierre@epitech.eu>
2026-04-15 09:42:48 +0200

    [Features] Editor
    Added the basis of my old editor, adapting it to our shell and coding style in progress

---
+++ src/cod_editor/launch.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2025
+** launch.c
+** File description:
+** This file launch the cod editor.
+*/
+#include "global.h"
+
+int cod(shell_parameters_t *shell)
+{
+    file_infos_t file_infos;
+    struct stat file_stat;
+    char *custom_file_name;
+    int ac = my_tablen(shell->command);
+
+    if (ac != 2) {
+        if (ac == 1) {
+            
+        } else {
+
+        }
+    }
+}
