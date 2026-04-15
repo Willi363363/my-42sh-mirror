670b494d8675535837de5199997cd6557b83ae98
Yaël Pierre <yael.pierre@epitech.eu>
2026-04-15 15:29:48 +0200

    [Features] Editor
    Added the basis of my old editor, adapting it to our shell and coding style in progress

--- src/cod_editor/launch.c
+++ src/cod_editor/launch.c
@@ -6,6 +6,23 @@
 */
 #include "global.h"
 
+static int option_check(shell_parameters_t *shell)
+{
+    if (strcmp(shell->command[1], "--help") == 0
+        || strcmp(shell->command[1], "--h") == 0) {
+        shell->last_exit_code = SUCCESS;
+        return editor_help(COMMAND_FOUND);
+    }
+    if (strcmp(shell->command[1], "--version") == 0
+        || strcmp(shell->command[1], "--v") == 0) {
+        shell->last_exit_code = SUCCESS;
+        return editor_version(COMMAND_FOUND);
+    }
+    shell->last_exit_code = EXIT_FAIL;
+    printf("%s: option not found. Try --h or --v.\n", shell->command[1]);
+    return COMMAND_ERROR;
+}
+
 int cod(shell_parameters_t *shell)
 {
     file_infos_t file_infos;
@@ -17,15 +34,7 @@ int cod(shell_parameters_t *shell)
         shell->last_exit_code = EXIT_FAIL;
         return editor_help(COMMAND_ERROR);
     }
-    if (strcmp(shell->command[1], "--help") == 0
-        || strcmp(shell->command[1], "--h") == 0) {
-        shell->last_exit_code = SUCCESS;
-        return editor_help(COMMAND_FOUND);
-    }
-    if (strcmp(shell->command[1], "--version") == 0
-        || strcmp(shell->command[1], "--v") == 0) {
-        shell->last_exit_code = SUCCESS;
-        return editor_version(COMMAND_FOUND);
-    }
+    if (strncmp(shell->command[1], "--", 2) == 0)
+        return option_check(shell);
     return COMMAND_FOUND;
 }
